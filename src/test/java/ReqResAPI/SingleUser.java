bd7d8df5dcfc26e8bb5f330c5957777038625587
Dheeraj Saini <90624377+SnakeEye15@users.noreply.github.com>
2025-07-22 23:45:18 +0530

    new project for api

---
+++ src/test/java/ReqResAPI/SingleUser.java
@@ -0,0 +1,27 @@
+package ReqResAPI;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SingleUser {
+    @Test
+    public void getSingleUser_Test1(){
+        baseURI="https://reqres.in";
+        given().
+                get("/api/users/2").
+                then().
+                statusCode(200);
+
+    }
+    @Test
+    public void singleUserNotFound_Test(){
+        baseURI="https://reqres.in";
+        given().
+                get("/api/users/23").
+                then().
+                statusCode(404);
+
+    }
+}
